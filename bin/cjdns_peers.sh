d1435f252d048480bbe55cd95a5206b972b2f868
makeworld <makeworld@protonmail.com>
2018-10-30 22:55:33 +0000

    added cjdns peer scripts, and changed config for cjdns peers

--- bin/cjdns_peers.sh
+++ bin/cjdns_peers.sh
@@ -1,4 +1,4 @@
-read -a peers <<< `sudo nodejs /opt/cjdns/tools/peerStats 2>/dev/null | awk '{ if ($2 == "ESTABLISHED") print $1 }' | awk -F. '{ print $6".k" }' | xargs`
+read -a peers <<< `sudo nodejs /opt/cjdns/tools/peerStats 2>/dev/null | awk '{ if ($3 == "ESTABLISHED") print $2 }' | awk -F. '{ print $6".k" }' | xargs`
 for peer in "${peers[@]}"; do
-    sudo /opt/cjdns/publictoip6 $peer
+    sudo /opt/cjdns/publictoip6 "$peer"
 done
